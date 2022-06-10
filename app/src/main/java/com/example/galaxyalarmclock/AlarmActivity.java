16812799c3263b1ffff5af6cd2b4d1882ea3234f
thpir <92860191+thpir@users.noreply.github.com>
2022-06-10 13:24:38 +0200

    Implemented first version of alarm manager in fragment 1

--- app/src/main/java/com/example/galaxyalarmclock/AlarmActivity.java
+++ app/src/main/java/com/example/galaxyalarmclock/AlarmActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
--- app/src/main/java/com/example/galaxyalarmclock/AlarmReceiver.java
+++ app/src/main/java/com/example/galaxyalarmclock/AlarmReceiver.java
@@ -4,24 +4,26 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.WindowManager;
 
 import androidx.core.app.NotificationCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Open the AlarmActivity
+        // Option 1 (doesn't work)
+        /*Intent alarmIntent = new Intent(context, AlarmActivity.class);
+        alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, alarmIntent, 0);
+        notificationBuilder.setContentIntent(pendingIntent);*/
+        // Option 2 (doesn't work when app is closed)
+        Intent alarmIntent = new Intent(context, AlarmActivity.class);
+        alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(alarmIntent);
         // Create a notification
         NotificationHelper notificationHelper = new NotificationHelper(context);
         NotificationCompat.Builder notificationBuilder = notificationHelper.getChannel1Notification("Galaxy Alarm Clock", "Alarm");
         notificationHelper.getManager().notify(1, notificationBuilder.build());
-
-        // Open the AlarmActivity
-        /*Intent i = new Intent(context, AlarmActivity.class);
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, 0);
-        notificationBuilder.setContentIntent(pendingIntent);*/
-        Intent alarmIntent = new Intent(context, AlarmActivity.class);
-        alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(alarmIntent);
     }
 }
